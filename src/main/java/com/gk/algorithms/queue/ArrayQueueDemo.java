c6a4c92235a2d74c476ff475005681cc945c08c4
BG407260 <kguo60@best-inc.com>
2022-01-25 22:59:16 +0800

    数组实现队列

--- src/main/java/com/gk/algorithms/queue/ArrayQueueDemo.java
+++ src/main/java/com/gk/algorithms/queue/ArrayQueueDemo.java
@@ -1,12 +1,48 @@
 package com.gk.algorithms.queue;
 
+import java.util.Scanner;
+
 /***
  * @author gk
  * @create 2022-01-25 10:31 下午
+ * 这种实现，数组只能使用一次，当rear=maxSize-1的时候，即使取出数据也没办法在加数据
  **/
 public class ArrayQueueDemo {
 
-    class ArrayQueue {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayQueue arrayQueue = new ArrayQueue(3);
+        boolean isContinue = true;
+        while (isContinue) {
+            System.out.println("请输入指令:");
+            System.out.println("a：增加元素");
+            System.out.println("g：获取元素");
+            System.out.println("h：队头数据");
+            System.out.println("all：所有数据");
+            System.out.println("e：退出");
+            String command = scanner.nextLine();
+            switch (command) {
+                case "a":
+                    System.out.println("请输入数据:");
+                    arrayQueue.add(scanner.nextInt());
+                    break;
+                case "g":
+                    System.out.println("数据：" + arrayQueue.get());
+                    break;
+                case "h":
+                    System.out.println("数据：" + arrayQueue.getHead());
+                    break;
+                case "all":
+                    arrayQueue.printQueue();
+                    break;
+                case "e":
+                    isContinue = false;
+                    break;
+            }
+        }
+    }
+
+    static class ArrayQueue {
         /**
          * 最低长度
          */
@@ -51,6 +87,23 @@ public class ArrayQueueDemo {
                 throw new RuntimeException("队列为空，请稍候重试");
             }
             return queue[++front];
+        }
+
+        public int getHead() {
+            if (isEmpty()) {
+                throw new RuntimeException("队列为空，请稍候重试");
+            }
+            return queue[front + 1];
+        }
+
+        public void printQueue() {
+            if (isEmpty()) {
+                return;
+            }
+            for (int i = 0; i < queue.length; i++) {
+                System.out.printf("queue[%d]=%d", i, queue[i]);
+                System.out.println();
+            }
         }
     }
 }
